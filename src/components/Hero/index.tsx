f4b2ebd82d33c953dda3290a959874da010953a2
felipendelicia <delicia4581@gmail.com>
2023-03-18 08:24:28 -0300

    stars in the hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <Container>
       <Text>
-        <Title>Todo para convertir tu casa en un HOGAR.</Title>
+        <Title>Todo para convertir tu casa en un HOGAR ✨</Title>
         <Paragraph>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi totam praesentium aliquid id quasi iure alias omnis voluptate, ab inventore velit nemo repellat nulla nihil optio. Consequatur fugit temporibus quia.</Paragraph>
         <LinkButton to='/buy'><FontAwesomeIcon icon={faAngleRight}/> Ver productos</LinkButton>
       </Text>
