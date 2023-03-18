1292304d19856009aca079affb01572ec3a6eb1b
felipendelicia <delicia4581@gmail.com>
2023-03-18 09:28:57 -0300

    email sender functionality

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,17 @@
+import sendEmail from "../services/sendEmail"
+
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className="pepe">
+      <button
+        onClick={()=>sendEmail(
+          'lucadelicia05@gmail.com',
+          'Test Email',
+          'Hello, my name is Felipe and i am testing Sendinblue'
+        )}
+      >
+        Send EMAIL
+      </button>
+    </div>
   )
 }
--- src/services/sendEmail.ts
+++ src/services/sendEmail.ts
@@ -2,15 +2,24 @@ import axios from 'axios'
 
 function sendEmail(to:string, subject:string, body:string){
     const data = {
-        to:[{email:to}],
+        sender: {
+            name: 'Luca Delicia',
+            email: to
+        },
+        to: [
+            {
+                email: 'delicia4581@gmail.com',
+                name: 'Felipe Delicia'
+            }
+        ],
         subject: subject,
-        htmlContent: body,
-        from: {email: 'delicia4581@gmail.com', name: 'Felipe Delicia'}
+        htmlContent: body
     }
     axios.post('https://api.sendinblue.com/v3/smtp/email', data, {
         headers: {
-            'Content-Type': 'application/json',
-            'api-key': process.env.REACT_APP_SENDINBLUE_API_KEY
+            'accept': 'application/json',
+            'api-key': process.env.REACT_APP_SENDINBLUE_API_KEY,
+            'content-type': 'application/json',
         }
     })
     .then(response => console.log(response))
