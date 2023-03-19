5a1c05315fb53fcbcf02cb581aa179b471fbb990
felipendelicia <delicia4581@gmail.com>
2023-03-19 18:41:41 -0300

    redirectz

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <Container>
       <Text>
         <Title>Todo para convertir tu casa en un HOGAR ✨</Title>
-        <Paragraph>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi totam praesentium aliquid id quasi iure alias omnis voluptate, ab inventore velit nemo repellat nulla nihil optio. Consequatur fugit temporibus quia.</Paragraph>
+        <Paragraph>En tesa tesa, creemos en la importancia de la satisfacción del cliente y nos esforzamos por brindarte una experiencia de compra sin igual. Ya sea que estés buscando un regalo para un ser querido o simplemente necesites algo para ti, tenemos lo que necesitas.</Paragraph>
         <LinkButton to='/buy'><FontAwesomeIcon icon={faAngleRight}/> Ver productos</LinkButton>
       </Text>
       <Image src="./assets/images/hero.jpg" alt="hero"/>
